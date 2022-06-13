24456f74e7d7d037ef6adfb98a1f3bd055eef12f
Alexandr Ksenz <AlexandrKsenz@gmail.com>
2022-06-13 16:44:22 +0200

    added subscribe

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // burger
 $(document).ready(function () {
+    $('.defense__close').click(function (event) {
+        $('.defense').css({
+            'display': 'none'
+        });
+    });
+});
+
+// burger
+$(document).ready(function () {
     $('.interaction__burger').click(function (event) {
         $('.interaction__menu,.interaction__burger').toggleClass('active');
         $('body').toggleClass('lock');
